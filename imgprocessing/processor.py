1b4dfe346e66bab0ac1ece16ea821fea9deec180
Mike Olasz <mikeolasz94@gmail.com>
2016-04-19 21:09:55 +0200

    add laplacian gradient

--- imgprocessing/processor.py
+++ imgprocessing/processor.py
@@ -4,3 +4,6 @@ import numpy as np
 class Processor():
     def detectCannyEdges(self,img):
         return cv2.Canny(img,100,200)
+        
+    def detectLaplacianEdges(self,img):
+        return cv2.Laplacian(img,cv2.CV_64F)
